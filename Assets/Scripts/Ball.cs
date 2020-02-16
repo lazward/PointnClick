aa6d869ac357d974882c751f86cfb030dbce8abb
lazward <lazwerd@gmail.com>
2020-02-16 02:23:42 -0500

    Started implementing ball pickup with mouse

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public bool isHeld = false ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isHeld) {
+
+            return ;
+
+        }
+
+        Vector3 pos = Input.mousePosition;
+        pos.z = transform.position.z - Camera.main.transform.position.z;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        pos.y = 8;
+        transform.position = pos;
+
+        if (Input.GetMouseButtonUp(0)) {
+            
+            isHeld = false ;
+
+        }
+        
+    }
+
+    void OnMouseDown() {
+
+        isHeld = true ;
+
+    }
+    
+}
