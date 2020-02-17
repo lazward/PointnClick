300d7a4419d05c3e25c77c52d92a3cdc9a873d7e
lazward <lazwerd@gmail.com>
2020-02-17 13:32:56 -0500

    Partially implemented separate goal functionality

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll) {
+
+        if ((this.gameObject.tag == "Red" && coll.gameObject.tag == "Red") || (this.gameObject.tag == "Blue" && coll.gameObject.tag == "Blue")) {
+
+            Destroy(coll.gameObject) ;
+
+        } else {
+
+            SceneManager.LoadScene("Scene0") ;
+
+        }
+
+    }
+
+}
